58d551223a67ee3549a87bf4b14aaa6d20466b94
adasu <supel.dev@gmail.com>
2022-05-12 19:19:26 +0200

    Initial commit

---
+++ src/main/java/com/example/sda_final_project/SdaFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sda_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sda_final_project/SdaFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sda_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
