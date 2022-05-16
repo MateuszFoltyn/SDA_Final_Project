c48496653d79ece383d00f6b2efff60067a41df7
Mateusz Foltyn <fol.mateusz@gmail.com>
2022-05-16 19:23:11 +0200

    made entity in Employee

--- src/main/java/com/example/sda_final_project/model/Employee.java
+++ src/main/java/com/example/sda_final_project/model/Employee.java
@@ -1,4 +1,22 @@
 package com.example.sda_final_project.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee extends Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    Integer deal;
+
 }
